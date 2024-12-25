3d82a719f9d18a4aaff1074d6380997a1f32e50a
Abhishek Choudhary <pmoabhishek@gmail.com>
2024-12-25 12:59:11 +0530

    Home Page

--- app/(landing-page)/page.tsx
+++ app/(landing-page)/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/navbar/navbar";
 import HeroSection from "./hero-section";
+import Section from "./sections";
 
 const LandingPage = () => {
   return (
     <div className="p-4">
       <Navbar />
       <HeroSection />
+      <Section />
     </div>
   );
 };
